da4d7fc3de1ceb2bda979492a82dce5ee9f81d24
Timer-2000 <120391297+Timer-2000@users.noreply.github.com>
2023-01-16 15:11:53 +0100

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 def Neopixel():
     if SCD30.read_co2() >= 100:
-        strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.YELLOW))
+        strip.set_pixel_color(0, neopixel.colors(NeoPixelColors.GREEN))
         strip.show()
     if SCD30.read_co2() >= 200:
         for Index in range(2):
-            strip.set_pixel_color(Index, neopixel.colors(NeoPixelColors.YELLOW))
+            strip.set_pixel_color(Index, neopixel.colors(NeoPixelColors.GREEN))
             strip.show()
     if SCD30.read_co2() >= 300:
         for Index2 in range(3):
-            strip.set_pixel_color(Index2, neopixel.colors(NeoPixelColors.YELLOW))
+            strip.set_pixel_color(Index2, neopixel.colors(NeoPixelColors.GREEN))
             strip.show()
     if SCD30.read_co2() >= 400:
         for Index3 in range(4):
-            strip.set_pixel_color(Index3, neopixel.colors(NeoPixelColors.YELLOW))
+            strip.set_pixel_color(Index3, neopixel.colors(NeoPixelColors.GREEN))
             strip.show()
     if SCD30.read_co2() >= 500:
         for Index4 in range(5):
@@ -32,19 +32,19 @@ def Neopixel():
             strip.show()
     if SCD30.read_co2() >= 900:
         for Index8 in range(9):
-            strip.set_pixel_color(Index8, neopixel.colors(NeoPixelColors.YELLOW))
+            strip.set_pixel_color(Index8, neopixel.colors(NeoPixelColors.RED))
             strip.show()
     if SCD30.read_co2() >= 1000:
         for Index9 in range(10):
-            strip.set_pixel_color(Index9, neopixel.colors(NeoPixelColors.YELLOW))
+            strip.set_pixel_color(Index9, neopixel.colors(NeoPixelColors.RED))
             strip.show()
     if SCD30.read_co2() >= 1100:
         for Index10 in range(11):
-            strip.set_pixel_color(Index10, neopixel.colors(NeoPixelColors.YELLOW))
+            strip.set_pixel_color(Index10, neopixel.colors(NeoPixelColors.RED))
             strip.show()
     if SCD30.read_co2() >= 1200:
         for Index11 in range(12):
-            strip.set_pixel_color(Index11, neopixel.colors(NeoPixelColors.YELLOW))
+            strip.set_pixel_color(Index11, neopixel.colors(NeoPixelColors.RED))
             strip.show()
 
 def on_pin_touch_p1():
@@ -102,7 +102,7 @@ def on_pin_touch_p1():
 input.on_pin_touch_event(TouchPin.P1, input.button_event_down(), on_pin_touch_p1)
 
 def on_button_a():
-    basic.show_string("De Bullinga")
+    pass
 input.on_button_event(Button.A, input.button_event_click(), on_button_a)
 
 def on_button_ab():
@@ -156,36 +156,6 @@ def on_button_ab():
     music.play_tone(1127, music.beat(BeatFraction.QUARTER))
 input.on_button_event(Button.AB, input.button_event_click(), on_button_ab)
 
-def on_button_b():
-    music.play_tone(262, music.beat(BeatFraction.HALF))
-    music.play_tone(294, music.beat(BeatFraction.HALF))
-    music.play_tone(330, music.beat(BeatFraction.HALF))
-    music.play_tone(349, music.beat(BeatFraction.HALF))
-    music.play_tone(392, music.beat(BeatFraction.WHOLE))
-    music.play_tone(392, music.beat(BeatFraction.WHOLE))
-    music.play_tone(440, music.beat(BeatFraction.HALF))
-    music.play_tone(440, music.beat(BeatFraction.HALF))
-    music.play_tone(440, music.beat(BeatFraction.HALF))
-    music.play_tone(440, music.beat(BeatFraction.HALF))
-    music.play_tone(392, music.beat(BeatFraction.DOUBLE))
-    music.play_tone(440, music.beat(BeatFraction.HALF))
-    music.play_tone(440, music.beat(BeatFraction.HALF))
-    music.play_tone(440, music.beat(BeatFraction.HALF))
-    music.play_tone(440, music.beat(BeatFraction.HALF))
-    music.play_tone(392, music.beat(BeatFraction.DOUBLE))
-    music.play_tone(349, music.beat(BeatFraction.HALF))
-    music.play_tone(349, music.beat(BeatFraction.HALF))
-    music.play_tone(349, music.beat(BeatFraction.HALF))
-    music.play_tone(349, music.beat(BeatFraction.HALF))
-    music.play_tone(330, music.beat(BeatFraction.WHOLE))
-    music.play_tone(330, music.beat(BeatFraction.WHOLE))
-    music.play_tone(392, music.beat(BeatFraction.HALF))
-    music.play_tone(392, music.beat(BeatFraction.HALF))
-    music.play_tone(392, music.beat(BeatFraction.HALF))
-    music.play_tone(392, music.beat(BeatFraction.HALF))
-    music.play_tone(262, music.beat(BeatFraction.WHOLE))
-input.on_button_event(Button.B, input.button_event_click(), on_button_b)
-
 def on_gesture_shake():
     music.play_tone(659, music.beat(BeatFraction.HALF))
     music.play_tone(659, music.beat(BeatFraction.HALF))
@@ -212,59 +182,14 @@ def on_gesture_shake():
     music.play_tone(440, music.beat(BeatFraction.WHOLE))
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+def on_pin_touch_p0():
+    pass
+input.on_pin_touch_event(TouchPin.P0, input.button_event_down(), on_pin_touch_p0)
+
 def on_pin_touch_p2():
-    global leben, spieler, hinderniss1, hinderniss2, hinderniss3, hinderniss4
-    leben = 1
-    basic.show_string("Flappy Birb for Calliope", 75)
-    spieler = game.create_sprite(0, 2)
-    hinderniss1 = game.create_sprite(4, randint(0, 4))
-    basic.pause(1000)
-    hinderniss1.change(LedSpriteProperty.X, -1)
-    hinderniss2 = game.create_sprite(4, randint(0, 4))
-    basic.pause(1000)
-    hinderniss1.change(LedSpriteProperty.X, -1)
-    hinderniss2.change(LedSpriteProperty.X, -1)
-    hinderniss3 = game.create_sprite(4, randint(0, 4))
-    basic.pause(1000)
-    hinderniss1.change(LedSpriteProperty.X, -1)
-    hinderniss2.change(LedSpriteProperty.X, -1)
-    hinderniss3.change(LedSpriteProperty.X, -1)
-    hinderniss4 = game.create_sprite(4, randint(0, 4))
-    basic.pause(1000)
-    hinderniss1.change(LedSpriteProperty.X, -1)
-    hinderniss2.change(LedSpriteProperty.X, -1)
-    hinderniss3.change(LedSpriteProperty.X, -1)
-    hinderniss4.change(LedSpriteProperty.X, -1)
-    while leben == 1:
-        basic.pause(1000)
-        hinderniss1.change(LedSpriteProperty.X, -1)
-        hinderniss2.change(LedSpriteProperty.X, -1)
-        hinderniss3.change(LedSpriteProperty.X, -1)
-        hinderniss4.change(LedSpriteProperty.X, -1)
-        if hinderniss1.is_touching_edge():
-            hinderniss1.set(LedSpriteProperty.X, 4)
-        if hinderniss2.is_touching_edge():
-            hinderniss2.set(LedSpriteProperty.X, 4)
-        if hinderniss3.is_touching_edge():
-            hinderniss3.set(LedSpriteProperty.X, 4)
-        if hinderniss4.is_touching_edge():
-            hinderniss4.set(LedSpriteProperty.X, 4)
-        if spieler.is_touching(hinderniss1):
-            leben = 0
-        if spieler.is_touching(hinderniss2):
-            leben = 0
-        if spieler.is_touching(hinderniss3):
-            leben = 0
-        if spieler.is_touching(hinderniss4):
-            leben = 0
+    pass
 input.on_pin_touch_event(TouchPin.P2, input.button_event_down(), on_pin_touch_p2)
 
-hinderniss4: game.LedSprite = None
-hinderniss3: game.LedSprite = None
-hinderniss2: game.LedSprite = None
-hinderniss1: game.LedSprite = None
-spieler: game.LedSprite = None
-leben = 0
 Würfel = 0
 strip: neopixel.Strip = None
 SCD30.set_calibration400ppm()
@@ -274,8 +199,8 @@ range2 = strip.range(0, 11)
 grove.setup_wifi(SerialPin.C17,
     SerialPin.C16,
     BaudRate.BAUD_RATE115200,
-    "Vodafone-7788",
-    "StepanIrinaTimMax")
+    "BYOD-WLAN",
+    "ESH_$1_Erdgeschoss")
 for Index12 in range(12):
     strip.set_pixel_color(Index12, neopixel.colors(NeoPixelColors.YELLOW))
     strip.show()
--- main.ts
+++ main.ts
@@ -1,73 +1,75 @@
 function Neopixel () {
     if (SCD30.readCO2() >= 100) {
-        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Yellow))
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
         strip.show()
+    } else {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 200) {
-        for (let Index = 0; Index <= 1; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+    } else {
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 300) {
-        for (let Index = 0; Index <= 2; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+    } else {
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 400) {
-        for (let Index = 0; Index <= 3; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        strip.show()
+    } else {
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 500) {
-        for (let Index = 0; Index <= 4; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Yellow))
+        strip.show()
+    } else {
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 600) {
-        for (let Index = 0; Index <= 5; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Yellow))
+        strip.show()
+    } else {
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 700) {
-        for (let Index = 0; Index <= 6; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Yellow))
+        strip.show()
+    } else {
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 800) {
-        for (let Index = 0; Index <= 7; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Yellow))
+        strip.show()
+    } else {
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 900) {
-        for (let Index = 0; Index <= 8; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    } else {
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 1000) {
-        for (let Index = 0; Index <= 9; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    } else {
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 1100) {
-        for (let Index = 0; Index <= 10; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    } else {
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Black))
     }
     if (SCD30.readCO2() >= 1200) {
-        for (let Index = 0; Index <= 11; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
-            strip.show()
-        }
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+    } else {
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Black))
     }
 }
 input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
@@ -129,7 +131,11 @@ input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
     }
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-	
+    if (leben == 1) {
+        spieler.change(LedSpriteProperty.Y, 1)
+    } else {
+        basic.showString("hi!")
+    }
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     music.playTone(523, music.beat(BeatFraction.Quarter))
@@ -181,6 +187,13 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     music.playTone(880, music.beat(BeatFraction.Quarter))
     music.playTone(1127, music.beat(BeatFraction.Quarter))
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    if (leben == 1) {
+        spieler.change(LedSpriteProperty.Y, -1)
+    } else {
+        basic.showString("hi!")
+    }
+})
 input.onGesture(Gesture.Shake, function () {
     music.playTone(659, music.beat(BeatFraction.Half))
     music.playTone(659, music.beat(BeatFraction.Half))
@@ -206,48 +219,9 @@ input.onGesture(Gesture.Shake, function () {
     music.playTone(523, music.beat(BeatFraction.Whole))
     music.playTone(440, music.beat(BeatFraction.Whole))
 })
-input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
 	
 })
-input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
-    leben = 1
-    basic.showString("Flappy Birb for Calliope", 75)
-    spieler = game.createSprite(1, 2)
-    hinderniss1 = game.createSprite(4, randint(1, 3))
-    basic.pause(1000)
-    hinderniss1.change(LedSpriteProperty.X, -1)
-    hinderniss2 = game.createSprite(4, randint(1, 3))
-    basic.pause(1000)
-    hinderniss1.change(LedSpriteProperty.X, -1)
-    hinderniss2.change(LedSpriteProperty.X, -1)
-    while (leben == 1) {
-        basic.pause(1000)
-        hinderniss1.change(LedSpriteProperty.X, -1)
-        hinderniss2.change(LedSpriteProperty.X, -1)
-        if (spieler.isTouching(hinderniss1)) {
-            leben = 0
-            spieler.delete()
-            hinderniss1.delete()
-            hinderniss2.delete()
-        }
-        if (spieler.isTouching(hinderniss2)) {
-            leben = 0
-            spieler.delete()
-            hinderniss1.delete()
-            hinderniss2.delete()
-        }
-        if (hinderniss1.isTouchingEdge()) {
-            hinderniss1.set(LedSpriteProperty.X, 4)
-            hinderniss1.set(LedSpriteProperty.Y, randint(1, 3))
-        }
-        if (hinderniss2.isTouchingEdge()) {
-            hinderniss2.set(LedSpriteProperty.X, 4)
-            hinderniss2.set(LedSpriteProperty.Y, randint(1, 3))
-        }
-    }
-})
-let hinderniss2: game.LedSprite = null
-let hinderniss1: game.LedSprite = null
 let spieler: game.LedSprite = null
 let leben = 0
 let Würfel = 0
@@ -255,16 +229,16 @@ let strip: neopixel.Strip = null
 SCD30.setCalibration400ppm()
 strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
 strip.setBrightness(20)
-let range = strip.range(0, 11)
+let range2 = strip.range(0, 11)
 grove.setupWifi(
 SerialPin.C17,
 SerialPin.C16,
 BaudRate.BaudRate115200,
-"Vodafone-7788",
-"StepanIrinaTimMax"
+"BYOD-WLAN",
+"ESH_$1_Erdgeschoss"
 )
-for (let Index = 0; Index <= 11; Index++) {
-    strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Yellow))
+for (let Index12 = 0; Index12 <= 11; Index12++) {
+    strip.setPixelColor(Index12, neopixel.colors(NeoPixelColors.Yellow))
     strip.show()
     basic.pause(500)
 }
